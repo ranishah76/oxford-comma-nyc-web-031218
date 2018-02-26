3052eb623dc9ee37e93b1dcf8da4b96fc36a80e3
Rani Shah <rani76@gmail.com>
2018-02-26 23:40:15 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
-  if array.length == 2
+  if array.length < 3
     return "#{array[0]} and #{array[1]}"
-  elsif 2 < array.length
+  else 
     array[-1].insert(0, "and ")
   end
   array.join(", ")
