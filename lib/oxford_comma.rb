db2b058371ee943047f2ce24f9675e0163c7ede2
Rani Shah <rani76@gmail.com>
2018-02-26 23:39:06 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,8 @@
 def oxford_comma(array)
-if array.length < 3
-  array.join(" and ")
-else
-  array.pop.join(", ") + " and " + array.last
-end
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")
 end
